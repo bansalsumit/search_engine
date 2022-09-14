d528de2bb739bfd41ff7b0fde41ff7edf9b596e6
Sumit Bansal <sbansal.mait@gmail.com>
2022-09-14 19:06:29 +0530

    Added general helpers.

---
+++ helpers/general.rb
@@ -0,0 +1,15 @@
+class General
+  def extract_keywords(str)
+    arr = str.split(' ')
+    arr.slice!(0)
+    arr
+  end
+
+  def is_page_input?(str)
+    str.start_with?('P')
+  end
+
+  def is_query_input?(str)
+    str.start_with?('Q')s
+  end
+end
